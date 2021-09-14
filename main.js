e2e62d5028b1498db127c1a3b30e44b1c136a072
ggroshansii <garthgroshansii@gmail.com>
2021-09-13 21:47:34 -0400

    finished styling for desktop viewing

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-const body = document.querySelector('body')
-const onLoadDiv = document.querySelector('.on-load')
+const body = document.querySelector('body');
+const onLoadDiv = document.querySelector('.on-load');
 const overallContainer = document.querySelector('.container');
+const imageBtn = document.querySelector('.image-btn');
+const logo1 = document.querySelector('.logo-1');
+const logo2 = document.querySelector('.logo-2');
 
-
+body.style.backgroundImage = "";
+overallContainer.style.display = "none";
+onLoadDiv.style.display = 'flex';
 window.addEventListener('load', (event) => {
-    body.style.backgroundImage = "";
-    overallContainer.style.display = "none";
-    onLoadDiv.style.display = 'flex';
     let onLoad = setTimeout(() => {
         body.style.backgroundImage = "url('https://shoutitoutdesign.com/wp-content/uploads/2016/10/90s-website-style.jpg')";
         overallContainer.style.display = 'block';
         onLoadDiv.style.display = 'none';
     }, 3500)
+  });
+
+  imageBtn.addEventListener('click', event => {
+    if (logo2.style.display === 'none') {
+        logo1.style.display = 'none';
+        logo2.style.display = 'block';
+    } else {
+        logo2.style.display = 'none';        
+        logo1.style.display = 'block';
+    }
 
   });
